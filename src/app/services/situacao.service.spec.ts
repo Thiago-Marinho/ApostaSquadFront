255d0f16651584557575eecb6a9486ff9f7d46c5
Misael Lima <misaellimadossantos@hotmail.com>
2021-10-07 10:51:57 -0400

    Alterando Situacao service

--- src/app/services/situacao.service.spec.ts
+++ src/app/services/situacao.service.spec.ts
@@ -1,111 +1,77 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { Situacao } from '../entities/situacao';
 import { SituacaoService } from './situacao.service';
 import * as $ from 'jquery'
-import { HttpClientModule } from '@angular/common/http';
+import { data } from 'jquery';
 
 describe('SituacaoService', () => {
+  let httpTestingController: HttpTestingController
   let service: SituacaoService;
+  
+  const url = "http://localhost:8080/situacao"
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule, HttpClientModule],
+      imports:[HttpClientTestingModule],
       providers:[SituacaoService]
     });
+    httpTestingController = TestBed.inject(HttpTestingController)
     service = TestBed.inject(SituacaoService);
   });
 
-  it('#listar deve retornar uma lista maior que zero', (done) => {
-    $.ajax({
-      url:'http://localhost:8080/situacao/listar',
-      dataType:'json',
-      success: (data:Situacao[], response:any)=>{
-        expect(data.length).toBeGreaterThanOrEqual(0)
-        done()
-      },
-      error: (data,response)=>{
-        expect(true).toThrow("Erro ao realizar teste")
-      }
-    })
-  });
-
-  it('#incluir deve adicionar um objeto',done=>{
-    const situacao:Situacao ={descricao:"Situacao de teste"}
-    let expected =0;
-    $.ajax({
-      url:'http://localhost:8080/situacao/listar',
-      dataType:'json',
-      success: (data:Situacao[], response:any)=>{
-        expected=data.length
-      },
-      error: (data,response)=>{
-        expect(true).toThrow("Erro ao realizar teste")
-      }
-    })
-
-    $.ajax({
-      type: "POST",
-      url: 'http://localhost:8080/situacao/incluir',
-      data: JSON.stringify(situacao),
-      success: success=>{
-        $.ajax({
-          url:'http://localhost:8080/situacao/listar',
-          dataType:'json',
-          success: (data:Situacao[], response:any)=>{
-            expect(data.length).toBeGreaterThan(expected)
-            done()
-          },
-          error: (data,response)=>{
-            expect(true).toThrow("Erro ao realizar teste")
-          }
-        })
-      },
-      dataType: 'json',
-      contentType: 'application/json; charset=utf-8'
-    });
-    done()
+  afterEach(() => {
+    httpTestingController.verify()
   })
 
-  it('#alterar deve atualizar um objeto',done=>{
-    const situacao:Situacao ={id:3, descricao:"Situacao de teste"}
-    let expected =0;
-
-    $.ajax({
-      type: "PUT",
-      url: 'http://localhost:8080/situacao/alterar',
-      data: JSON.stringify(situacao),
-      success: success=>{
-        $.ajax({
-          url:'http://localhost:8080/situacao/listar',
-          dataType:'json',
-          success: (data:Situacao[], response:any)=>{
-            expect(data.length).toBeGreaterThan(expected)
-            done()
-          },
-          error: (data,response)=>{
-            expect(true).toThrow("Erro ao realizar teste")
-          }
-        })
-      },
-      dataType: 'json',
-      contentType: 'application/json; charset=utf-8'
-    });
-    done()
+  it("#Listar - Deve retornar uma lista de Situacao", () =>{
+    const listaSituacao:Situacao[] = [
+      {id: 1, descricao: "Acertou"},
+      {id: 2, descricao: "Errou"}
+    ]
+    service.listar().subscribe(
+      data => {expect(data).toEqual(listaSituacao)}
+    )
+    const testRequest = httpTestingController.expectOne(url + '/listar')
+    expect(testRequest.request.method).toBe('GET')
+    testRequest.flush(listaSituacao)
   })
 
-  it('#carregarSituacao deve retornar um objeto válido',(done)=>{
-    $.ajax({
-      url:'http://localhost:8080/situacao/2',
-      dataType:'json',
-      success: (data:Situacao, response:any)=>{
-        expect(data==null).toEqual(false)
-        done()
-      },
-      error: (data,response)=>{
-        expect(true).toThrow("Erro ao realizar teste")
-      }
-    })
+  it("#carregarEstadio deve fazer a requisição por um item específico, a partir do método GET", ()=>{
+    const situcaoTest:Situacao = {id: 1, descricao: "Pendente"}
+    
+    service.carregarSituacao(1).subscribe(
+      data=>{expect(data).toEqual(situcaoTest), console.log("carregar", data)}
+      
+    )
+    const testRequest = httpTestingController.expectOne(url + "/"+ situcaoTest.id)
+    expect(testRequest.request.method).toBe('GET')
+    testRequest.flush(situcaoTest)
+  })
+
+  it("#incluir - deve enviar um objeto do tipo Situacao", ()=>{
+    const situcaoTest:Situacao = {id: 3, descricao: "Pendente"}
+
+    service.incluir(situcaoTest).subscribe(
+      data => expect(data).toEqual(situcaoTest)
+    )
+    const testrequest = httpTestingController.expectOne(url + '/incluir')
+    expect(testrequest.request.method).toBe('POST')
+    expect(testrequest.request.body.descricao).toEqual(situcaoTest.descricao)
+    console.log(testrequest.request.body)
+    testrequest.flush(situcaoTest)
+  })
+
+  it("#alterar - Deve alterar um objeto do tipo Situacao", () => {
+    const situcaoTest:Situacao = {id: 1, descricao: "Pendente"}
+    service.alterar(situcaoTest).subscribe(
+      data => {expect(data).toEqual(situcaoTest), console.log("data: ", data)}
+    )
+    const testrequest = httpTestingController.expectOne(url + '/alterar')
+    expect(testrequest.request.method).toBe('PUT')
+    expect(testrequest.request.body.id).toEqual(situcaoTest.id)
+    expect(testrequest.request.body.descricao).toEqual(situcaoTest.descricao)
+    testrequest.flush(situcaoTest)
   })
 });
 
