c43fbc6489b45fb80a2f20c505e1e684e9f0c601
Misael Lima <misaellimadossantos@hotmail.com>
2021-10-08 10:24:49 -0400

    Implementando teste nos componentes aposta e aposta-alterar

--- src/app/components/aposta-alterar/aposta-alterar.component.spec.ts
+++ src/app/components/aposta-alterar/aposta-alterar.component.spec.ts
@@ -1,4 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApostaService } from 'src/app/services/aposta.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { PartidaService } from 'src/app/services/partida.service';
+import { ResultadoApostaService } from 'src/app/services/resultado-aposta.service';
+import { SituacaoService } from 'src/app/services/situacao.service';
+import { TimePartidaService } from 'src/app/services/time-partida.service';
+import { TimeService } from 'src/app/services/time.service';
 
 import { ApostaAlterarComponent } from './aposta-alterar.component';
 
@@ -8,6 +19,17 @@ describe('ApostaAlterarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        ApostaService, 
+        SituacaoService, 
+        ClienteService,
+        PartidaService,
+        TimeService,
+        TimePartidaService,
+        ResultadoApostaService,
+        FormBuilder
+      ],
       declarations: [ ApostaAlterarComponent ]
     })
     .compileComponents();
@@ -19,7 +41,25 @@ describe('ApostaAlterarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('#Create - O componente deve ser criado', () => {
     expect(component).toBeTruthy();
   });
+
+  it('#Title - O titulo deve ser criado', () => {
+    const app = fixture.debugElement.nativeElement
+    let h1 = app.querySelector('#title')
+    let expected: string = "Alterar aposta do"
+    let result: string = h1.innerHTML
+
+    expect(result).toContain(expected)
+  })
+
+  it('#BotaoSalvar - Deve aparecer no html', () => {
+    const app = fixture.debugElement.nativeElement
+    let button = app.querySelector('#salvar')
+    let expected: string = "Salvar"
+    let result: string = button.innerHTML
+
+    expect(result).toContain(expected)
+  })
 });
--- src/app/components/aposta/aposta.component.spec.ts
+++ src/app/components/aposta/aposta.component.spec.ts
@@ -1,4 +1,15 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { ApostaService } from 'src/app/services/aposta.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { PartidaService } from 'src/app/services/partida.service';
+import { ResultadoApostaService } from 'src/app/services/resultado-aposta.service';
+import { SituacaoService } from 'src/app/services/situacao.service';
+import { TimePartidaService } from 'src/app/services/time-partida.service';
+import { TimeService } from 'src/app/services/time.service';
 
 import { ApostaComponent } from './aposta.component';
 
@@ -8,6 +19,17 @@ describe('ApostaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [
+        ApostaService, 
+        SituacaoService, 
+        ClienteService,
+        PartidaService,
+        TimeService,
+        TimePartidaService,
+        ResultadoApostaService,
+        FormBuilder
+      ],
       declarations: [ ApostaComponent ]
     })
     .compileComponents();
@@ -19,7 +41,14 @@ describe('ApostaComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('#Create - O componente deve ser criado', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#Title - deve ter um titulo', () =>{
+    const app: DebugElement = fixture.debugElement;
+    const title = app.query(By.css('h1'));
+    const titleElement: HTMLElement = title.nativeElement
+    expect(titleElement.textContent).toContain('Listar Apostas')
   });
 });
