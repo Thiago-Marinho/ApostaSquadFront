20615ec8845e8c0bd622f2c2403a9718c93c067e
Marcus Vinicius R. Oliveira <moliveira@itelinfo.com>
2021-10-08 10:12:40 -0400

    Situacao componente

--- src/app/components/situacao/situacao.component.spec.ts
+++ src/app/components/situacao/situacao.component.spec.ts
@@ -1,13 +1,21 @@
+import { SituacaoService } from './../../services/situacao.service';
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SituacaoComponent } from './situacao.component';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('SituacaoComponent', () => {
   let component: SituacaoComponent;
   let fixture: ComponentFixture<SituacaoComponent>;
+  let title: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [SituacaoService, FormBuilder],
       declarations: [ SituacaoComponent ]
     })
     .compileComponents();
@@ -22,4 +30,18 @@ describe('SituacaoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('#Title - O titulo deve constar no html', () => {
+
+    const app = fixture.debugElement.nativeElement;
+    title = app.querySelector("#title");
+
+    let expected: string = "Situação";
+    let result: string = title.innerHTML
+
+    expect(result).toContain(expected);
+
+  });
+
 });
