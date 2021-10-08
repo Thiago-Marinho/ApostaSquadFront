0699d02b1e591cb8070f85311ca6878b30fbcb79
emersonnobre <egrnobre@gmail.com>
2021-10-08 10:18:51 -0400

    criacao de testes para o component de resultadoAposta e resultadoApostaAlterar

--- src/app/components/resultado-aposta/resultado-aposta.component.spec.ts
+++ src/app/components/resultado-aposta/resultado-aposta.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { ResultadoApostaService } from 'src/app/services/resultado-aposta.service';
 
 import { ResultadoApostaComponent } from './resultado-aposta.component';
 
@@ -8,6 +11,8 @@ describe('ResultadoApostaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [ResultadoApostaService, FormBuilder],
       declarations: [ ResultadoApostaComponent ]
     })
     .compileComponents();
@@ -19,7 +24,7 @@ describe('ResultadoApostaComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('#Create - Deve criar um component de ResultadoAposta', () => {
     expect(component).toBeTruthy();
   });
 });
