c03b64eae4fe4ca33a97a90585f6a31cf69282fb
Lucas D. Soares <lsoares@itelinfo.com>
2021-10-08 10:26:24 -0400

    Implementação do TimePartida Teste

--- src/app/components/time-partida-alterar/time-partida-alterar.component.spec.ts
+++ src/app/components/time-partida-alterar/time-partida-alterar.component.spec.ts
@@ -1,25 +1,42 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { identifierModuleUrl } from '@angular/compiler';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TimePartidaService } from 'src/app/services/time-partida.service';
 
 import { TimePartidaAlterarComponent } from './time-partida-alterar.component';
 
 describe('TimePartidaAlterarComponent', () => {
   let component: TimePartidaAlterarComponent;
   let fixture: ComponentFixture<TimePartidaAlterarComponent>;
+  let h1: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TimePartidaAlterarComponent ]
+      imports : [HttpClientTestingModule, RouterTestingModule],
+      providers : [TimePartidaService, FormBuilder], 
+      declarations : [ TimePartidaAlterarComponent ]
     })
     .compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(TimePartidaAlterarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('#Create - O componente deve ser criado', () => {
     expect(component).toBeTruthy();
   });
+
+  it('#Title - O titulo deve constar no html', () => {
+    
+    const app = fixture.debugElement.nativeElement;
+    h1 = app.querySelector("#title");
+
+    let expected: string = "Atualizar Relação entre time e partida do";
+    let result: string = h1.innerHTML
+
+    expect(result).toContain(expected);
+  }); 
 });
--- src/app/components/time-partida/time-partida.component.spec.ts
+++ src/app/components/time-partida/time-partida.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { TimePartidaService } from 'src/app/services/time-partida.service';
 
 import { TimePartidaComponent } from './time-partida.component';
 
@@ -8,6 +13,8 @@ describe('TimePartidaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [TimePartidaService, FormBuilder],
       declarations: [ TimePartidaComponent ]
     })
     .compileComponents();
@@ -21,5 +28,12 @@ describe('TimePartidaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#title deve ter aquele titulo', () => {
+    const app: DebugElement = fixture.debugElement;
+    const title = app.query(By.css('h1'));
+    const titleElement: HTMLElement = title.nativeElement;
+    expect(titleElement.textContent).toContain('Lista de timePartida')
   });
 });
