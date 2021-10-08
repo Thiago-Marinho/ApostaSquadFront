cb2c6f0d6521e1034a0cf40904257f59b472059c
SidneyMaldonado <sidneymaldonado@gmail.com>
2021-10-08 09:42:29 -0400

    Testar componente

--- src/app/components/cliente-alterar/cliente-alterar.component.spec.ts
+++ src/app/components/cliente-alterar/cliente-alterar.component.spec.ts
@@ -1,25 +1,56 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ClienteService } from './../../services/cliente.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ClienteAlterarComponent } from './cliente-alterar.component';
 
 describe('ClienteAlterarComponent', () => {
+
   let component: ClienteAlterarComponent;
   let fixture: ComponentFixture<ClienteAlterarComponent>;
-
+  let h1: HTMLElement;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports : [HttpClientTestingModule, RouterTestingModule],
+      providers: [ClienteService],
       declarations: [ ClienteAlterarComponent ]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(ClienteAlterarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
-  it('should create', () => {
+
+  it('#Create - O componente deve ser criado', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('#Title - O titulo deve constar no html', () => {
+
+    const app = fixture.debugElement.nativeElement;
+    h1 = app.querySelector("#title");
+
+    let expected: string = "Alterar cliente do";
+    let result: string = h1.innerHTML
+
+    expect(result).toContain(expected);
+
+  });
+
+  it('#BotaoSalvar - Deve aparecer no html', () => {
+    const app = fixture.debugElement.nativeElement;
+    h1 = app.querySelector("#salvar");
+
+    let expected: string = "Salvar";
+    let result: string = h1.innerHTML
+
+    expect(result).toContain(expected);
+  });
+
+
 });
--- src/app/components/cliente/cliente.component.spec.ts
+++ src/app/components/cliente/cliente.component.spec.ts
@@ -1,6 +1,11 @@
+import { ClienteService } from './../../services/cliente.service';
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClienteComponent } from './cliente.component';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ClienteComponent', () => {
   let component: ClienteComponent;
@@ -8,6 +13,8 @@ describe('ClienteComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [ClienteService, FormBuilder],
       declarations: [ ClienteComponent ]
     })
     .compileComponents();
@@ -22,4 +29,14 @@ describe('ClienteComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('#title deve ter quele titulo', () => {
+    const app: DebugElement = fixture.debugElement;
+    const title= app.query(By.css('h1'));
+    const titleElement: HTMLElement = title.nativeElement;
+    expect(titleElement.textContent).toContain('Listar Clientes');
+  });
+
+
 });
