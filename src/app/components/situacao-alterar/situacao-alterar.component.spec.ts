07bf2b84cf4485ee47b4a7fd255c1f28cf0123a8
Marcus Vinicius R. Oliveira <moliveira@itelinfo.com>
2021-10-08 10:02:10 -0400

    Situacao alterrar teste componente

--- src/app/components/situacao-alterar/situacao-alterar.component.spec.ts
+++ src/app/components/situacao-alterar/situacao-alterar.component.spec.ts
@@ -1,25 +1,45 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SituacaoService } from './../../services/situacao.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SituacaoAlterarComponent } from './situacao-alterar.component';
 
 describe('SituacaoAlterarComponent', () => {
+
   let component: SituacaoAlterarComponent;
   let fixture: ComponentFixture<SituacaoAlterarComponent>;
-
+  let h1: HTMLElement;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports : [HttpClientTestingModule, RouterTestingModule],
+      providers: [SituacaoService],
       declarations: [ SituacaoAlterarComponent ]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(SituacaoAlterarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('#Create - O componente deve ser criado', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#Title - O titulo deve constar no html', () => {
+    const app = fixture.debugElement.nativeElement;
+    h1 = app.querySelector("#title");
+    let expected: string = "Alterar situação do";
+    let result: string = h1.innerHTML
+    expect(result).toContain(expected);
+  });
+
+  it('#BotaoSalvar - Deve aparecer no html', () => {
+    const app = fixture.debugElement.nativeElement;
+    h1 = app.querySelector("#salvar");
+    let expected: string = "Salvar";
+    let result: string = h1.innerHTML
+    expect(result).toContain(expected);
   });
 });
