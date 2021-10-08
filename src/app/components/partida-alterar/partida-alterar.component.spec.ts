45dd57c4b945d71dc60066af80dd5ce12f50fb23
Thiago M. Peres <tperes@itelinfo.com>
2021-10-08 10:41:08 -0400

    Teste em componente de Partida

--- src/app/components/partida-alterar/partida-alterar.component.spec.ts
+++ src/app/components/partida-alterar/partida-alterar.component.spec.ts
@@ -1,13 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ClienteService } from './../../services/cliente.service';
 import { PartidaAlterarComponent } from './partida-alterar.component';
+import { PartidaService } from 'src/app/services/partida.service';
+import { EstadioService } from 'src/app/services/estadio.service';
 
 describe('PartidaAlterarComponent', () => {
   let component: PartidaAlterarComponent;
   let fixture: ComponentFixture<PartidaAlterarComponent>;
+  let h1: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports : [HttpClientTestingModule, RouterTestingModule],
+      providers: [PartidaService, EstadioService],
       declarations: [ PartidaAlterarComponent ]
     })
     .compileComponents();
@@ -19,7 +26,30 @@ describe('PartidaAlterarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('#Create - O componente deve ser criado', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('#Title - O titulo deve constar no html', () => {
+
+    const app = fixture.debugElement.nativeElement;
+    h1 = app.querySelector("#title");
+
+    let expected: string = "Alterar partida do";
+    let result: string = h1.innerHTML
+
+    expect(result).toContain(expected);
+
+  });
+
+  it('#BotaoSalvar - Deve aparecer no html', () => {
+    const app = fixture.debugElement.nativeElement;
+    h1 = app.querySelector("#salvar");
+
+    let expected: string = "Salvar";
+    let result: string = h1.innerHTML
+
+    expect(result).toContain(expected);
   });
 });
--- src/app/components/partida/partida.component.spec.ts
+++ src/app/components/partida/partida.component.spec.ts
@@ -1,13 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PartidaComponent } from './partida.component';
+import { EstadioService } from 'src/app/services/estadio.service';
+import { PartidaService } from 'src/app/services/partida.service';
+
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { By } from '@angular/platform-browser';
 
 describe('PartidaComponent', () => {
   let component: PartidaComponent;
   let fixture: ComponentFixture<PartidaComponent>;
-
+  let h1: HTMLElement;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [PartidaService, EstadioService, FormBuilder],
       declarations: [ PartidaComponent ]
     })
     .compileComponents();
@@ -19,7 +28,14 @@ describe('PartidaComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('#Create - O componente deve ser criado', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#Title - O titulo deve constar no html', () => {
+    const app = fixture.debugElement.nativeElement;
+    const title = app.querySelector('#title')
+
+    expect(title.innerHTML).toContain("Listar Partidas")
   });
 });
